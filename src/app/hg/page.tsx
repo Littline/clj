cee30265b629798d2894d33bbcd98fd48b2bac45
jiuzheng.su <jiuzheng_su@163.com>
2024-10-15 17:15:12 +0800

    海工项目

--- src/app/hg/page.tsx
+++ src/app/hg/page.tsx
@@ -43,11 +43,45 @@ export default function Home() {
       tension: 0.1,
     }]
   });
+  const [inputValue, setInputValue] = useState(""); // 用户输入
+  const [responseData, setResponseData] = useState(null); // 接收后端返回的数据
+  
 
   // 状态变量用于存储最新接收到的消息
   const [messageLog, setMessageLog] = useState([]);
   // 状态变量用于存储当前时间
   const [currentTime, setCurrentTime] = useState('');
+  const [loading, setLoading] = useState(false); // 加载状态
+    // 提交表单
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 设置加载状态为true
+    setLoading(true);
+
+    // 调用后端HTTP服务的URL
+    const url = 'http://127.0.0.1:8091/handleMessage'; // 替换为实际的后端API地址
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ input: inputValue }), // 将输入转化为JSON字符串
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setResponseData(data); // 将返回的数据存储
+      console.log("Received fetching data:", data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    })
+    .finally(() => {
+      // 请求完成后，无论成功与否，隐藏加载提示
+      setLoading(false);
+    });
+  };
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:8090');
@@ -58,7 +92,7 @@ export default function Home() {
     };
 
     ws.onmessage = (event) => {
-      console.log('收到消息:', event.data);
+      // console.log('收到消息:', event.data);
 
       // 将新消息添加到日志中
       setMessageLog((prevLog) => {
@@ -155,6 +189,37 @@ export default function Home() {
           ))}
         </ul>
       </div>
+      <div className="container">
+        {/* 用户输入表单 */}
+        <form onSubmit={handleSubmit} className="input-form">
+          <label htmlFor="input">输入数据：</label>
+          <input
+            type="text"
+            id="input"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            style={{ color: 'black' }}
+          />
+          <button type="submit">提交</button>
+        </form>
+
+        {/* 展示加载提示 */}
+        {loading && (
+          <div className="loading">
+            <p>加载中，请稍候...</p>
+          </div>
+        )}
+
+        {/* 展示返回的结果 */}
+        {responseData && !loading && (
+          <div className="response-data">
+            <h2>返回结果：</h2>
+            <p>{JSON.stringify(responseData)}</p>
+          </div>
+        )}
+      </div>
+
     </div>
+    
   );
 }
